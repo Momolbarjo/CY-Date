f4dff0c2d48cf3bbf24b0e52f446ebd44912d723
Mohamed ben <119139743+Momolbarjo@users.noreply.github.com>
2024-04-13 20:35:05 +0200

    Create picture.js

---
+++ login/picture.js
@@ -0,0 +1,14 @@
+document.getElementById('imageUpload').addEventListener('change', function (e) {
+    const img = document.querySelector('.round-image');
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    if (file && ['image/png', 'image/jpeg', 'image/gif'].includes(file.type)) {
+        reader.onloadend = function () {
+            img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+    } else {
+        alert('Please upload a jpg, a jpeg ,a png or a gif file');
+    }
+});
