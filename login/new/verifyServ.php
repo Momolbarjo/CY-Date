8a2721129fd8d24e598b3cc7fb5f5fba1ce2174d
Mohamed <benlaifao@cy-tech.fr>
2024-04-15 18:24:11 +0200

    d

--- login/new/verifyServ.php
+++ login/new/verifyServ.php
@@ -11,19 +11,16 @@ function verify_user_data()
 
     if (!preg_match('/^[a-zA-Z]+$/', $surname)) {
         $_SESSION['error'] = 'Surname should be with  letters only';
-        error_log("Error: " . $_SESSION['error']);
         return false;
     }
 
     if (!preg_match('/^[a-zA-Z]+$/', $name)) {
         $_SESSION['error'] = 'Name should be with  letters only';
-        error_log("Error: " . $_SESSION['error']);
         return false;
     }
 
     if (!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/', $password)) {
         $_SESSION['error'] = 'Your password need to be at least 8 characters with at least : uppercase letter,lowercase letter, a number and a special character ';
-        error_log("Error: " . $_SESSION['error']);
         return false;
     }
 
@@ -34,7 +31,6 @@ function verify_user_data()
 
     if ($age < 18) {
         $_SESSION['error'] = 'You need to be at least 18';
-        error_log("Error: " . $_SESSION['error']);
         return false;
     }
 
@@ -45,13 +41,11 @@ function verify_user_data()
 
         if ($existingUsername == $username) {
             $_SESSION['error'] = 'Username already exist';
-            error_log("Error: " . $_SESSION['error']);
             return false;
         }
 
         if ($existingEmail == $email) {
             $_SESSION['error'] = 'An account with this email already exist';
-            error_log("Error: " . $_SESSION['error']);
             return false;
         }
     }
