e50e2d762ce2f60fed925a3826e77a7471f0f9cd
Mohamed <benlaifao@cy-tech.fr>
2024-04-17 20:17:24 +0200

    screen vh

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Login</title>
     <link rel="stylesheet" href="login/log.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
