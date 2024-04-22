b8795d32852d3d8808a58dbb54f9d5b8fe42fe5b
Mohamed <benlaifao@cy-tech.fr>
2024-04-22 23:07:27 +0200

    icons

--- web/dashboard.php
+++ web/dashboard.php
@@ -13,9 +13,9 @@
     <header class="navMenu">
         <a href="#"><img class="logo" src="../Pictures/cupid.png"></a>
         <nav class="navigationBar">
-            <a href="logOut.php">Log Out</a>
-            <a href="#">Search</a>
-            <a href="#">Subscription</a>
+            <a href="logOut.php">Log Out<i class='bx bxs-log-out-circle'></i></a>
+            <a href="#">Search<i class='bx bx-search-alt' ></i></a>
+            <a href="#">Subscription<i class='bx bx-money' ></i></a>
             <label for="imageUpload">
                 <img src="<?php session_start();  if(!$_SESSION['role']){header('Location: ../index.php');}echo $_SESSION['profile_pic']; ?>" class="round-image" alt="cantFoundPic">
             </label>
