82a00b78171da043e79cefc9a5913422b39b3305
Mohamed <benlaifao@cy-tech.fr>
2024-04-23 23:40:19 +0200

    input verif

--- login/verifyServ.php
+++ login/verifyServ.php
@@ -10,32 +10,39 @@ function verify_user_data()
     $password = $_POST["password"];
     $gender =$_POST["gender"];
 
-    if (!preg_match('/^[a-zA-Z]+$/', $surname)) {
-        $_SESSION['error'] = '⚠️Surname should be with  letters only⚠️';
+    if (!preg_match('/^[a-zA-Z]{1,12}$/', $surname)) {
+        $_SESSION['error'] = '⚠️Surname should be with  letters only and between 1 to 12 characters⚠️';
         return false;
     }
 
-    if (!preg_match('/^[a-zA-Z]+$/', $name)) {
-        $_SESSION['error'] = '⚠️Name should be with  letters only⚠️';
+    if (!preg_match('/^[a-zA-Z]{1,12}$/', $name)) {
+        $_SESSION['error'] = '⚠️Name should be with  letters only and between 1 to 12 characters⚠️';
         return false;
     }
 
-    $gender = strtolower($gender);
-
-    if ($gender != "male" && $gender != "female" && $gender != "others") {
-        $_SESSION['error'] = '⚠️Your gender  should be male , female or others only⚠️';
+    if (strlen($email) > 30) {
+        $_SESSION['error'] = '⚠️Your email should not exceed 15 characters⚠️';
         return false;
-    } 
+    }
 
+    
     if(!preg_match("~@gmail\.com$~",$email)){
         $_SESSION['error'] = '⚠️You should use an @gmail account⚠️';
         return false;
     }
 
-    if (!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/', $password)) {
-        $_SESSION['error'] = '⚠️Your password need to be at least 8 characters with at least : uppercase letter,lowercase letter, a number and a special character⚠️ ';
+
+    if (!preg_match('/^(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z,])(?!.*\s).{8,16}$/', $password)) {
+        $_SESSION['error'] = '⚠️Your password need to be at least 8 (up to 16) characters with at least : uppercase letter,lowercase letter, a number and a special character (, excluded)⚠️ ';
         return false;
     }
+
+    if (!preg_match('/^[^,\s]{1,10}$/', $username))  {
+        $_SESSION['error'] = '⚠️Your username should not contain a comma and be at the most 10 characters long⚠️';
+        return false;
+    }
+    
+
 
     $birthday = new DateTime($birthday);
     $now = new DateTime();
