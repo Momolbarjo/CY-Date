a0dde9d2df3ac12c0586926db48173e07c3003a2
Mohamed <benlaifao@cy-tech.fr>
2024-04-23 20:41:13 +0200

    admin stuff

--- admin/Dash.php
+++ admin/Dash.php
@@ -10,5 +10,13 @@
 </head>
 <body>
     <?php session_start(); if(!$_SESSION['role']){header('Location: ../index.php');}?>
+    <header class="navMenu">
+        <a href="#"><img class="logo" src="../Pictures/cupid.png"></a>
+        <nav class="navigationBar">
+            <a href="../web/logOut.php">Log Out<i class='bx bxs-log-out-circle'></i></a>
+            <a href="#">All users<i class='bx bx-user-x'></i></a>
+            <a href="#">Banned<i class='bx bx-cross' ></i></a>
+        </nav>
+    </header>
 </body>
 </html>
