9559dfe88ca8030b33dc55e67f210cba5ba1fa02
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-05-10 11:41:47 +0200

    Update profilePage.js

--- web/profile/profilePage.js
+++ web/profile/profilePage.js
@@ -1,4 +1,3 @@
-
 document.getElementById('optionsBtn').addEventListener('click', function () {
 	var optionsMenu = document.getElementById('optionsMenu');
 	if (optionsMenu.style.display === 'none') {
@@ -9,24 +8,64 @@ document.getElementById('optionsBtn').addEventListener('click', function () {
 });
 
 var count = 1;
-function changeImg(i) {
+
+function changeImg(i,max) {
+	
+	maximum = parseInt(max);
 	var image_pre = document.getElementById('img' + count);
 	var nextCount = count + i;
 
-
-	if (nextCount < 1) {
-		count = totalImages;
-	} else if (nextCount > totalImages) {
+	count++;
+	if (count > maximum) {
 		count = 1;
-	} else {
-		count = nextCount;
 	}
-
+	if (count < 1) {
+		count = maximum;
+	}
+	
+	
+	alert
 	var image_new = document.getElementById('img' + count);
 
-	document.body.style.backgroundImage = "url('" + image_new.src + "')";
-
 	image_pre.style.visibility = "hidden";
+	image_new.style.visibility = "visible";
 }
 
 
+var state = 0;
+
+
+function zoom(max) {
+	
+	maximum = parseInt(max)
+	if (state == 0) {
+		state = 1;
+		document.getElementById("profile").style.visibility = "hidden";
+		document.getElementById("btn3").style.visibility = "visible";
+		document.getElementById("btn4").style.visibility = "visible";
+		for (x=1;x<=maximum;x++) {
+				document.getElementById('img' + x ).style.left = '-5%';
+				document.getElementById('img' + x ).style.bottom = '3%';
+				document.getElementById('img' + x ).style.height = '80%';
+				document.getElementById('img' + x ).style.width = '80%';
+		}		
+	}
+	
+	else if (state == 1) {
+		
+		state = 0;
+		document.getElementById("profile").style.visibility = "visible";
+		document.getElementById("btn3").style.visibility = "hidden";
+		document.getElementById("btn4").style.visibility = "hidden";
+		for (y=1;y<=maximum;y++) {
+		
+			document.getElementById('img' + y ).style.left = '42.5%';
+			document.getElementById('img' + y ).style.bottom = '20%';
+			document.getElementById('img' + y ).style.height = '20%';
+			document.getElementById('img' + y ).style.width = '20%';
+		}
+		
+	}
+
+
+}
