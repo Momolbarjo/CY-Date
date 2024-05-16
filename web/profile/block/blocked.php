ab7b9348c892d7499b9ce2f71d73d34baebe5552
Malik <126243406+Daccors@users.noreply.github.com>
2024-05-16 19:39:29 +0200

    Add files via upload

---
+++ web/profile/block/blocked.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blocked Message</title>
+    <link rel="stylesheet" href="blocked.css"> 
+</head>
+<body>
+    <div class="blocked-message">
+        <p>You have been blocked by this user, maybe you were too rude?</p>
+    </div>
+</body>
+</html>
