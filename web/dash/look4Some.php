e91440e4d28b62fd882ca4a963dddfa6729297a6
Mohamed <benlaifao@cy-tech.fr>
2024-05-17 19:51:59 +0200

    block

--- web/dash/look4Some.php
+++ web/dash/look4Some.php
@@ -13,17 +13,24 @@ if(isset($_GET['i'])){
             $profilePicPath = $line[9];
 
             if($username !== $_SESSION['input_log']['username']) {
+                $isBlocked = false;
                 foreach($lines as $line2){
                     $data = str_getcsv($line2);
-                    if($data[0] == $_SESSION['input_log']['username'] && $data[1] == $username){
-                        echo "<a href='../profile/block/blocker.php?user=$username'><div class='profile'><img class='roundOther-image' src='$profilePicPath' alt='Profile Picture'><p>$username</p></div></a>";
+                    if(count($data) >= 2) {
+                        if($data[0] == $_SESSION['input_log']['username'] && $data[1] == $username){
+                            echo "<a href='../profile/block/blocker.php?user=$username'><div class='profile'><img class='roundOther-image' src='$profilePicPath' alt='Profile Picture'><p>$username</p></div></a>";
+                            $isBlocked = true;
+                            break;
+                        }
+                        elseif($data[1] == $_SESSION['input_log']['username'] && $data[0] == $username){
+                            echo "<a href='../profile/block/blocked.php?user=$username'><div class='profile'><img class='roundOther-image' src='$profilePicPath' alt='Profile Picture'><p>$username</p></div></a>";
+                            $isBlocked = true;
+                            break;
+                        }
                     }
-                    elseif($data[1] == $_SESSION['input_log']['username'] && $data[0] == $username){
-                        echo "<a href='../profile/block/blocked.php?user=$username'><div class='profile'><img class='roundOther-image' src='$profilePicPath' alt='Profile Picture'><p>$username</p></div></a>";
-                    }
-                    else{
-                        echo "<a href='../profile/profilePage.php?user=$username'><div class='profile'><img class='roundOther-image' src='$profilePicPath' alt='Profile Picture'><p>$username</p></div></a>";
-                    }
+                }
+                if(!$isBlocked){
+                    echo "<a href='../profile/profilePage.php?user=$username'><div class='profile'><img class='roundOther-image' src='$profilePicPath' alt='Profile Picture'><p>$username</p></div></a>";
                 }
                 
             }
