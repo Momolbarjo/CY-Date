c2e503c398b219c60b456935561ef3559e773ac4
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-05-26 15:11:54 +0200

    Update infos.js

--- web/profile/infos.js
+++ web/profile/infos.js
@@ -1,12 +1,12 @@
-
 function deleteAccount(state) {
-
-	if (state == 0) {
-		document.getElementById("settings").style.visibility = "hidden";
-		document.getElementById("deleteMenu").style.visibility = "visible";
-	} else {
-		document.getElementById("settings").style.visibility = "visible";
-		document.getElementById("deleteMenu").style.visibility = "hidden";
-	}
+    // Vérifie la valeur de 'state'
+    if (state == 0) {
+        // Si 'state' est 0, cache l'élément avec l'ID "settings" et affiche l'élément avec l'ID "deleteMenu".
+        document.getElementById("settings").style.visibility = "hidden";
+        document.getElementById("deleteMenu").style.visibility = "visible";
+    } else {
+        // Sinon, affiche l'élément avec l'ID "settings" et cache l'élément avec l'ID "deleteMenu".
+        document.getElementById("settings").style.visibility = "visible";
+        document.getElementById("deleteMenu").style.visibility = "hidden";
+    }
 }
-
