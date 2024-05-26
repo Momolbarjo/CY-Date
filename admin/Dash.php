7a67f0f8bcf5aa9474875114bf412a089142ab91
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2024-05-26 13:48:03 +0200

    Update Dash.php

--- admin/Dash.php
+++ admin/Dash.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php /* Programme HTML/CSS/PHP/JS permettant d'afficher la bar de navigation en haut du site dans le menu principal. */ ?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
